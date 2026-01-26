4f1ce00d899b3869c23a2620709bec94f783d232
Natalia Borovskikh <lai.tash@gmail.com>
2026-01-26 15:29:29 +0000

    Add tariff schema

---
+++ tariff_fetch/genability/schema/common.py
@@ -0,0 +1,42 @@
+from typing import Literal
+
+TariffPrivacy = Literal["PUBLIC", "UNLISTED", "PRIVATE"]
+
+LoadServingServiceType = Literal["ELECTRICITY", "SOLAR_PV"]
+TariffServiceType = Literal[LoadServingServiceType, "GAS"]
+TariffType = Literal["DEFAULT", "ALTERNATIVE", "OPTIONAL_EXTRA", "RIDER"]
+CustomerClass = Literal["RESIDENTIAL", "GENERAL", "SPECIAL_USE"]
+TariffEffectiveOnRule = Literal["TARIFF_EFFECTIVE_DATE", "BILLING_PERIOD_START"]
+TariffChargeType = Literal[
+    "FIXED_PRICE",
+    "CONSUMPTION_BASED",
+    "DEMAND_BASED",
+    "QUANTITY",
+    "FORMULA",
+    "MINIMUM",
+    "MAXIMUM",
+    "TAX",
+]
+TariffChargePeriod = Literal["MONTHLY", "DAILY", "QUARTERLY", "ANNUALLY"]
+
+RateChargeClass = Literal[
+    "SUPPLY",
+    "TRANSMISSION",
+    "DISTRIBUTION",
+    "TAX",
+    "CONTRACTED",
+    "USER_ADJUSTED",
+    "AFTER_TAX",
+    "OTHER",
+    "NON_BYPASSABLE",
+]
+RateTransactionType = Literal["BUY", "SELL", "NET", "BUY_IMPORT", "SELL_IMPORT"]
+RateUnit = Literal["COST_PER_UNIT", "PERCENTAGE"]
+
+TerritoryUsageType = Literal["SERVICE", "TARIFF", "CLIMATE_ZONE", "UTILITY_CLIMATE_ZONE"]
+TerritoryItemType = Literal["STATE", "COUNTY", "CITY", "ZIPCODE"]
+
+SeasonPredominance = Literal["PREDOMINANT", "SUBSERVIENT"]
+
+TimeOfUseType = Literal["SUPER_OFF_PEAK", "OFF_PEAK", "PARTIAL_PEAK", "ON_PEAK", "SUPER_ON_PEAK", "CRITICAL_PEAK"]
+TimeOfUsePrivacy = Literal["PUBLIC", "PRIVATE"]
---
+++ tariff_fetch/genability/schema/tariffproperty.py
@@ -0,0 +1,77 @@
+from typing import Literal, NotRequired, TypedDict
+
+from pydantic.alias_generators import to_camel
+
+TariffPropertyFormulaType = Literal["FORMULA"]
+
+
+TariffPropertyPrunedDataType = Literal[
+    "STRING",
+    "CHOICE",
+    "BOOLEAN",
+    "DATE",
+    "DECIMAL",
+    "INTEGER",
+    "DEMAND",
+]
+
+TariffPropertyDataType = Literal[
+    TariffPropertyPrunedDataType,
+    TariffPropertyFormulaType,
+]
+
+TariffPropertyCategory = Literal[
+    "APPLICABILITY",
+    "RATE_CRITERIA",
+    "BENEFIT",
+    "DATA_REPUTATION",
+    "SERVICE_TERMS",
+]
+
+
+TariffPropertyPeriod = Literal[
+    "ON_PEAK",
+    "PARTIAL_PEAK",
+    "OFF_PEAK",
+    "CRITICAL_PEAK",
+]
+
+
+class TariffPropertyChoice(TypedDict):
+    value: str
+    "Machine readable option value"
+    displayValue: str
+    "Human readable value shown to end users"
+    dataValue: str
+    likelihood: NotRequired[float | None]
+
+
+class TariffPropertyMinimalFields(TypedDict):
+    quantity_key: NotRequired[str | None]
+    quantity_unit: NotRequired[str | None]
+    key_name: str
+    display_name: str
+    keyspace: str
+    family: str
+    description: str
+    data_type: TariffPropertyDataType
+    property_types: TariffPropertyCategory
+
+
+class TariffPropertyStandardFields(TypedDict):
+    period: NotRequired[TariffPropertyPeriod]
+    operator: str | None
+    propertyValue: NotRequired[str]
+    minValue: NotRequired[str]
+    maxValue: NotRequired[str]
+    choices: NotRequired[list[TariffPropertyChoice]]
+    formula_detail: NotRequired[str]
+    is_default: bool
+
+
+class TariffPropertyMinimal(TariffPropertyMinimalFields):
+    __pydantic_config__ = {"alias_generator": to_camel, "extra": "allow"}  # pyright: ignore[reportGeneralTypeIssues, reportUnannotatedClassAttribute]  # noqa
+
+
+class TariffPropertyStandard(TariffPropertyMinimalFields, TariffPropertyStandardFields):
+    __pydantic_config__ = {"alias_generator": to_camel, "extra": "allow"}  # pyright: ignore[reportGeneralTypeIssues, reportUnannotatedClassAttribute]  # noqa
