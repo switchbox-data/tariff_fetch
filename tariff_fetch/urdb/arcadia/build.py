585120845ed26fb81e75fcc0dd6590b9ab830500
Natalia Borovskikh <lai.tash@gmail.com>
2026-03-04 12:23:39 +0000

    bump version

--- tariff_fetch/urdb/arcadia/build.py
+++ tariff_fetch/urdb/arcadia/build.py
@@ -1,10 +1,14 @@
 from dataclasses import dataclass
+from typing import cast
+
+import questionary
 
 from tariff_fetch.arcadia.api import ArcadiaSignalAPI
 from tariff_fetch.urdb.arcadia.library import Library
 from tariff_fetch.urdb.schema import URDBRate
 
 from .energyschedule import build_energy_schedule
+from .fixedcharge import build_fixed_charge
 from .scenario import Scenario
 
 
@@ -15,5 +19,25 @@ class URDBChunk:
 
 def build_urdb(api: ArcadiaSignalAPI, scenario: Scenario) -> URDBRate:
     library = Library(api)
-    energy_schedule = build_energy_schedule(scenario, library)
-    return {**energy_schedule}
+    try:
+        energy_schedule = build_energy_schedule(scenario, library)
+    except Exception as e:
+        energy_schedule = _confirm_proceed(e, "energy rate strucutre")
+
+    try:
+        fixed_charge = build_fixed_charge(scenario, library)
+    except Exception as e:
+        fixed_charge = _confirm_proceed(e, "fixed charges")
+    return {**energy_schedule, **fixed_charge}
+
+
+def _confirm_proceed(e: Exception, processing: str) -> URDBRate:
+    response = cast(
+        bool | None,
+        questionary.confirm(f"Error while converting: {processing}: {e}. Continue or print traceback and exit?").ask(),
+    )
+    if response is None:
+        exit()
+    if response:
+        return {}
+    raise e from None
